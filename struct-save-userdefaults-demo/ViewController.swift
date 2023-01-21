839d231e10e3012927602852b90a52504a2b5bf1
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-01-21 23:35:59 +0900

    Initial Commit

---
+++ struct-save-userdefaults-demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  struct-save-userdefaults-demo
+//
+//  Created by JotaroSugiyama on 2023/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
