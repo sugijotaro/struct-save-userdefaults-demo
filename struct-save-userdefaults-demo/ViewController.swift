c95982b11aee64b654098c7fcb5a0630297e9027
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-01-22 00:06:38 +0900

    pickerView実装

--- struct-save-userdefaults-demo/ViewController.swift
+++ struct-save-userdefaults-demo/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var thumbnailImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        thumbnailImageView.image = UIImage(systemName: "photo.circle",withConfiguration: UIImage.SymbolConfiguration(font: .systemFont(ofSize: 48)))
+        thumbnailImageView.isUserInteractionEnabled = true
+        thumbnailImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imageViewTapped(_:))))
     }
 
-
+    @objc func imageViewTapped(_ sender: UITapGestureRecognizer) {
+        let imagePickerController: UIImagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = UIImagePickerController.SourceType.photoLibrary
+        self.present(imagePickerController, animated: true, completion: nil)
+        imagePickerController.delegate = self
+     }
+    
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as? UIImage
+        thumbnailImageView.image = image
+        thumbnailImageView.contentMode = .scaleAspectFit
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
