61677231a38e6425ce0d77c8190934667dd7bc59
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2023-01-22 00:31:47 +0900

    保存部分を作成

--- struct-save-userdefaults-demo/ViewController.swift
+++ struct-save-userdefaults-demo/ViewController.swift
@@ -9,15 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var descriptionTextField: UITextField!
     @IBOutlet var thumbnailImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        thumbnailImageView.image = UIImage(systemName: "photo.circle",withConfiguration: UIImage.SymbolConfiguration(font: .systemFont(ofSize: 48)))
+        resetInputs()
         thumbnailImageView.isUserInteractionEnabled = true
         thumbnailImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(imageViewTapped(_:))))
     }
+    
+    @IBAction func saveButtonTapped() {
+        if let title = titleTextField.text, let description = descriptionTextField.text, let thumbnail = thumbnailImageView.image {
+            let movie = Movie(title: title, description: description, thumbnail: thumbnail.jpegData(compressionQuality: 1)!)
+            addMovies(movie: movie, forKey: "movies")
+            showAlert(message: "保存完了")
+        } else {
+            showAlert(message: "すべての項目を埋めてください")
+        }
+    }
+
 
     @objc func imageViewTapped(_ sender: UITapGestureRecognizer) {
         let imagePickerController: UIImagePickerController = UIImagePickerController()
@@ -25,6 +38,26 @@ class ViewController: UIViewController {
         self.present(imagePickerController, animated: true, completion: nil)
         imagePickerController.delegate = self
      }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: message, message: nil, preferredStyle: .alert)
+        present(alert, animated: true, completion: nil)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func resetInputs() {
+        titleTextField.text = ""
+        descriptionTextField.text = ""
+        thumbnailImageView.image = UIImage(systemName: "photo.circle",withConfiguration: UIImage.SymbolConfiguration(font: .systemFont(ofSize: 48)))
+    }
+    
+    func addMovies(movie: Movie,forKey: String) {
+        var movies = loadMoies(forKey: forKey)
+        movies?.append(movie)
+        saveMovies(movies: movies!, forKey: forKey)
+    }
     
 }
 
